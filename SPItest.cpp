b0ab2fc3d6fad0bf259211bccb61c667d6b339a9
Batonreal <batonreal@gmail.com>
2025-03-13 15:21:48 +0300

    v2.1

--- SPItest.cpp
+++ SPItest.cpp
@@ -20,35 +20,35 @@ class SpiController {
         SpiController(const std::string& device) : devicePath(device) {
             openDevice();
         }
-    
+
         ~SpiController() {
             closeDevice();
         }
-    
+
         void setSpeed(uint32_t speedHz) {
             if (ioctl(fileDescriptor, SPI_IOC_WR_MAX_SPEED_HZ, &speedHz) == -1) {
                 throw std::runtime_error("Ошибка при установке скорости SPI");
             }
         }
-    
+
         std::vector<uint8_t> transfer(const std::vector<uint8_t>& txData) {
             std::vector<uint8_t> rxData(txData.size());
-        
+
             spi_ioc_transfer transfer;
             memset(&transfer, 0, sizeof(transfer)); // Инициализация нулями
-        
+
             transfer.tx_buf = (unsigned long)txData.data();
             transfer.rx_buf = (unsigned long)rxData.data();
             transfer.len = txData.size();
             transfer.speed_hz = 0; // Используем установленную скорость
-        
+
             if (ioctl(fileDescriptor, SPI_IOC_MESSAGE(1), &transfer) < 0) {
                 throw std::runtime_error("Ошибка при передаче SPI");
             }
-        
+
             return rxData;
         }
-    
+
     private:
         void openDevice() {
             fileDescriptor = open(devicePath.c_str(), O_RDWR);
@@ -58,13 +58,13 @@ class SpiController {
                 throw std::runtime_error(ss.str());
             }
         }
-    
+
         void closeDevice() {
             if (fileDescriptor != -1) {
                 close(fileDescriptor);
             }
         }
-    
+
         std::string devicePath;
         int fileDescriptor = -1;
 };
@@ -112,17 +112,34 @@ void handleSignal(int signal) {
     stop = 1;
 }
 
+std::vector<uint8_t> createMessage(const std::vector<uint32_t>& values) {
+    std::vector<uint8_t> message;
+    for (uint32_t value : values) {
+        message.push_back((value >> 24) & 0xFF);
+        message.push_back((value >> 16) & 0xFF);
+        message.push_back((value >> 8) & 0xFF);
+        message.push_back(value & 0xFF);
+    }
+    return message;
+}
+
 void continuousTransfer(SpiController& spi) {
-    std::vector<uint8_t> send = {0xAA}; // 10101010 in binary
+	uint32_t value_1 = 0x75BCD15;
+	uint32_t value_2 = 0x3ADE68B1;
+	uint32_t value_3 = 0xACACAAAA;
+    std::vector<uint32_t> values = {value_1, value_2, value_3}; // Пример значений
+
+    // Создаем сообщение из значений
+    std::vector<uint8_t> send = createMessage(values);
 
     signal(SIGINT, handleSignal); // Register signal handler for Ctrl+C
 
-    uint32_t speedHz = 100000; // 100 kHz
+    uint32_t speedHz = 500000; // 500 kHz
     spi.setSpeed(speedHz); // Установить тактовую частоту
 
     while (!stop) {
         spi.transfer(send);
-        sleep(1); // Wait for 1 second
+        sleep(0.1); // Wait for 1 second
     }
 }
 
